cc5712974a8b0b3921043620eee8165748307ad1
JD-edu <jdedu.kr@gmail.com>
2024-03-10 22:17:18 +0900

    first commit

---
+++ arduino_listen/src/arduino_listen.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+using std::placeholders::_1;
+
+class ArduinoSubscriber : public rclcpp::Node
+{
+  public:
+    ArduinoSubscriber()
+    : Node("arduino_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+      "micro_ros_arduino_node_publisher", 10, std::bind(&ArduinoSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::Int32 & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg.data);
+    }
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ArduinoSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ ros_topic_py/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros_topic_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros2',
+    maintainer_email='ros2@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = ros_topic_py.ros_topic_py_pub:main',
+                'listener = ros_topic_py.ros_topic_py_sub:main',
+        ],
+    },
+)
